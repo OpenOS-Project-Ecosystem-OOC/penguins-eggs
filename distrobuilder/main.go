geaaru/distrobuilder

Permit definition of environment vars on image file
Signed-off-by: Daniele Rondina <geaaru@sabayonlinux.org>

distrobuilder/main.go
@@ -197,7 +197,7 @@ func (c *cmdGlobal) preRunBuild(cmd *cobra.Command, args []string) error {
 	}
 
 	// Setup the mounts and chroot into the rootfs
-	exitChroot, err := shared.SetupChroot(c.sourceDir)
+	exitChroot, err := shared.SetupChroot(c.sourceDir, c.definition.EnvVariables)
 	if err != nil {
 		return fmt.Errorf("Failed to setup chroot: %s", err)
 	}

distrobuilder/main_lxc.go
@@ -66,7 +66,8 @@ func (c *cmdLXC) run(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	exitChroot, err := shared.SetupChroot(c.global.sourceDir)
+	exitChroot, err := shared.SetupChroot(c.global.sourceDir,
+		c.global.definition.EnvVariables)
 	if err != nil {
 		return err
 	}

distrobuilder/main_lxd.go
@@ -85,7 +85,8 @@ func (c *cmdLXD) run(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	exitChroot, err := shared.SetupChroot(c.global.sourceDir)
+	exitChroot, err := shared.SetupChroot(c.global.sourceDir,
+		c.global.definition.EnvVariables)
 	if err != nil {
 		return err
 	}

OpenOS-Project-Ecosystem-OOC/penguins-eggs

distrobuilder/main.go
@@ -197,7 +197,7 @@ func (c *cmdGlobal) preRunBuild(cmd *cobra.Command, args []string) error {
 	}
 
 	// Setup the mounts and chroot into the rootfs
-	exitChroot, err := shared.SetupChroot(c.sourceDir)
+	exitChroot, err := shared.SetupChroot(c.sourceDir, c.definition.Environment)
 	if err != nil {
 		return fmt.Errorf("Failed to setup chroot: %s", err)
 	}

distrobuilder/main_lxc.go
@@ -66,7 +66,8 @@ func (c *cmdLXC) run(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	exitChroot, err := shared.SetupChroot(c.global.sourceDir)
+	exitChroot, err := shared.SetupChroot(c.global.sourceDir,
+		c.global.definition.Environment)
 	if err != nil {
 		return err
 	}

distrobuilder/main_lxd.go
@@ -85,7 +85,8 @@ func (c *cmdLXD) run(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	exitChroot, err := shared.SetupChroot(c.global.sourceDir)
+	exitChroot, err := shared.SetupChroot(c.global.sourceDir,
+		c.global.definition.Environment)
 	if err != nil {
 		return err
 	}